648a1a6e6d02b24d2bdb0d93e7dcf97b59c81447
AlwinSunil <alv12alwin@gmail.com>
2021-04-01 19:12:53 +0530

    v1.0 - Implemented Desktop layout.

--- app/js/script.js
+++ app/js/script.js
@@ -5,47 +5,8 @@ window.onload = function () {
   loader.style.display = "none";
   document.getElementById("body").style.height = "auto";
   document.getElementById("body").style.overflow = "auto";
+  addAnimation();
 };
-
-const scrollElements = document.querySelectorAll(".js-scroll");
-
-const elementInView = (el, dividend = 1) => {
-  const elementTop = el.getBoundingClientRect().top;
-  return (
-    elementTop <=
-    (window.innerHeight || document.documentElement.clientHeight) / dividend
-  );
-};
-
-const elementOutofView = (el) => {
-  const elementTop = el.getBoundingClientRect().top;
-
-  return (
-    elementTop > (window.innerHeight || document.documentElement.clientHeight)
-  );
-};
-
-const displayScrollElement = (element) => {
-  element.classList.add("scrolled");
-};
-
-const hideScrollElement = (element) => {
-  element.classList.remove("scrolled");
-};
-
-const handleScrollAnimation = () => {
-  scrollElements.forEach((el) => {
-    if (elementInView(el, 1.25)) {
-      displayScrollElement(el);
-    } else if (elementOutofView(el)) {
-      hideScrollElement(el);
-    }
-  });
-};
-
-window.addEventListener("scroll", () => {
-  handleScrollAnimation();
-});
 
 function checkbox() {
   var checkBox = document.getElementById("checkbox");
@@ -61,14 +22,13 @@ function checkbox() {
 }
 
 function warning() {
-  window.scroll({top: 0, behavior: "smooth"});
-  document.getElementById("close-button").style.display ="block";
+  window.scroll({ top: 0, behavior: "smooth" });
+  document.getElementById("close-button").style.display = "block";
   document.getElementById("close-button").style.fontSize = "1rem";
-  document.getElementById("container").style.opacity= "50%";
-
+  document.getElementById("container").style.opacity = "50%";
 }
 
 function closeWarning() {
-    document.getElementById("close-button").style.fontSize = "0px";
-    document.getElementById("container").style.opacity= "100%";
+  document.getElementById("close-button").style.fontSize = "0px";
+  document.getElementById("container").style.opacity = "100%";
 }
